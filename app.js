cb6637df501a21b03b720dc28d5cf1727377d1c0
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2021-12-16 23:49:52 -0800

    Style notifications light

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const closeAlertButton = document.querySelector('.close-button');
+const notificationsLight = document.querySelector('.js-notifications-light');
+const bellIcon = document.querySelector('.js-bell');
 
 closeAlertButton.addEventListener('click', () => {
   const alert = document.querySelector('.alert');
   alert.style.display = 'none';
+});
+
+bellIcon.addEventListener('mouseover', () => {
+  notificationsLight.style.transform = 'scale(1.3)';
+});
+
+bellIcon.addEventListener('mouseout', () => {
+  notificationsLight.style.transform = 'unset';
 });
