f98a4407897a4f7468219318842e09fb68b30c43
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-01-21 15:25:06 -0800

    Rename trafficData variable to trafficWeeklyData

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ const notifications = [
 
 const trafficWeeklyLabels = ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31',];
 
-const trafficData = {
-  labels: trafficLabels,
+const trafficWeeklyData = {
+  labels: trafficWeeklyLabels,
   datasets: [{
     data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
     backgroundColor: 'rgba(116, 119, 191, .3)',
